bc57ae462b060933f4620742199f9d59028bab67
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-14 18:29:47 +0200

    Expand Vector functionality

--- Raytracer/Vector3.cs
+++ Raytracer/Vector3.cs
@@ -1,22 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Raytracer
 {
-    struct Vector3
+    internal struct Vector3
     {
+// ReSharper disable InconsistentNaming
         public float x;
         public float y;
         public float z;
+// ReSharper restore InconsistentNaming
 
         public Vector3(float x, float y, float z)
         {
             this.x = x;
             this.y = y;
             this.z = z;
+        }
+
+        public float LengthSquared()
+        {
+            return (x*x + y*y + z*z);
+        }
+
+        public float Length()
+        {
+            return (float)Math.Sqrt(LengthSquared());
+        }
+
+        public Vector3 Normalized()
+        {
+            float len = Length();
+            return new Vector3(x/len, y/len, z/len);
+        }
+
+        public static Vector3 Zero
+        {
+            get { return new Vector3(0.0f, 0.0f, 0.0f); }
+        }
+
+        public static float Dot(Vector3 lhs, Vector3 rhs)
+        {
+            return lhs.x*rhs.x +
+                   lhs.y*rhs.y +
+                   lhs.z*rhs.z;
+        }
+
+        public static Vector3 Cross(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.y*rhs.z - lhs.z*rhs.y,
+                lhs.z*rhs.x - lhs.x*rhs.z,
+                lhs.x*rhs.y - lhs.y*rhs.x);
+        }
+
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
         }
     }
 }
