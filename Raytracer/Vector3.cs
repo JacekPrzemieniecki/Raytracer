98b90c680f51637c68f4a6f0395e6148b94d3a1f
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-18 10:35:37 +0200

    Formatting vector as string, code cleanup

--- Raytracer/Vector3.cs
+++ Raytracer/Vector3.cs
@@ -1,14 +1,15 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Raytracer
 {
     internal struct Vector3
     {
-// ReSharper disable InconsistentNaming
+        // ReSharper disable InconsistentNaming
         public float x;
         public float y;
         public float z;
-// ReSharper restore InconsistentNaming
+        // ReSharper restore InconsistentNaming
 
         public Vector3(float x, float y, float z)
         {
@@ -19,7 +20,7 @@ namespace Raytracer
 
         public float LengthSquared()
         {
-            return (x*x + y*y + z*z);
+            return (x * x + y * y + z * z);
         }
 
         public float Length()
@@ -30,7 +31,15 @@ namespace Raytracer
         public Vector3 Normalized()
         {
             float len = Length();
-            return new Vector3(x/len, y/len, z/len);
+            return new Vector3(x / len, y / len, z / len);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Vector3 {0}, {1}, {2}",
+                x.ToString("+##0.0;-##0.0", CultureInfo.InvariantCulture),
+                y.ToString("+##0.0;-##0.0", CultureInfo.InvariantCulture),
+                z.ToString("+##0.0;-##0.0", CultureInfo.InvariantCulture));
         }
 
         public static Vector3 Zero
@@ -40,21 +49,21 @@ namespace Raytracer
 
         public static float Dot(Vector3 lhs, Vector3 rhs)
         {
-            return lhs.x*rhs.x +
-                   lhs.y*rhs.y +
-                   lhs.z*rhs.z;
+            return lhs.x * rhs.x +
+                   lhs.y * rhs.y +
+                   lhs.z * rhs.z;
         }
 
         public static Vector3 Cross(Vector3 lhs, Vector3 rhs)
         {
-            return new Vector3(lhs.y*rhs.z - lhs.z*rhs.y,
-                lhs.z*rhs.x - lhs.x*rhs.z,
-                lhs.x*rhs.y - lhs.y*rhs.x);
+            return new Vector3(lhs.y * rhs.z - lhs.z * rhs.y,
+                lhs.z * rhs.x - lhs.x * rhs.z,
+                lhs.x * rhs.y - lhs.y * rhs.x);
         }
 
         public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
         {
-            return  new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+            return new Vector3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
         }
 
         public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
