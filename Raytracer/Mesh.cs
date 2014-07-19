d4f9068e905f6bce29e1bbaf5aba24b2ce01381b
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-19 15:32:45 +0200

    Remove testing vertex data

--- Raytracer/Mesh.cs
+++ Raytracer/Mesh.cs
@@ -15,17 +15,7 @@ namespace Raytracer
             TriangleCount = Triangles.Length / 3;
         }
 
-        public Mesh()
-            : this(new[] {
-                new Vector3(0.0f, 0.0f, -5.0f), 
-                new Vector3(3.0f, 3.0f, -5.0f), 
-                new Vector3(0.0f, 3.0f, -5.0f),
-                new Vector3(-1.0f, 0.0f, -3.0f),
-                new Vector3(2.0f, 0.0f, -3.0f),
-                new Vector3(1.75f, 1.0f, -3.0f)  
-                },
-                new[] { 0, 1, 2, 3, 4, 5 }
-                )
+        protected Mesh()
         {
         }
 
