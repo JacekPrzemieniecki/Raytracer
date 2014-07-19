fca71431b62f5833532772699090c9506d8707b2
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-19 18:10:48 +0200

    Add debug/perf counters #2

---
+++ Raytracer/Debugging/Counters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raytracer.Debugging
+{
+    static class Counters
+    {
+        public static int RaysCast;
+        public static int RayTriangleTests;
+    }
+}
