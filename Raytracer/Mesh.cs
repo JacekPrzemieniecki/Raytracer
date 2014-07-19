2fb2f965aa7c27f1ca4b52cd68f7135097cb95ca
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-19 15:19:38 +0200

    Extract ray tracing to mesh

--- Raytracer/Mesh.cs
+++ Raytracer/Mesh.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Drawing;
 
 namespace Raytracer
 {
@@ -27,6 +27,56 @@ namespace Raytracer
                 new[] { 0, 1, 2, 3, 4, 5 }
                 )
         {
+        }
+
+        /// <summary>
+        /// Casts a ray against mesh
+        /// </summary>
+        /// <param name="ray">Ray to be cast</param>
+        /// <param name="color">Color of the point where ray hit</param>
+        /// <param name="maxDistance">Maximum distance to trace ray</param>
+        /// <returns>Distance along the ray the hit was found</returns>
+        public float Raycast(Ray ray, ref Color color, float maxDistance)
+        {
+            var closestHit = new RaycastHit { t = maxDistance };
+            for (int i = 0; i < TriangleCount; i++)
+            {
+                RaycastHit hitInfo;
+                if (RaycastTriangle(i, ray, out hitInfo) && (closestHit.t > hitInfo.t))
+                {
+                    closestHit = hitInfo;
+                }
+            }
+// ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (closestHit.t < maxDistance)
+            {
+                color = Color.Red;
+            }
+            return closestHit.t;
+        }
+
+        private bool RaycastTriangle(int triangleId, Ray ray, out RaycastHit hitInfo)
+        {
+            hitInfo = new RaycastHit();
+            int triangleOffset = triangleId * 3;
+            Vector3 vert0 = Vertices[Triangles[triangleOffset]];
+            Vector3 vert1 = Vertices[Triangles[triangleOffset + 1]];
+            Vector3 vert2 = Vertices[Triangles[triangleOffset + 2]];
+            Vector3 edge1 = vert1 - vert0;
+            Vector3 edge2 = vert2 - vert0;
+            Vector3 pVec = Vector3.Cross(ray.Direction, edge2);
+            float determinant = Vector3.Dot(edge1, pVec);
+// ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (determinant == 0) return false;
+            float invDeterminant = 1 / determinant;
+            Vector3 tVec = ray.Origin - vert0;
+            hitInfo.u = Vector3.Dot(tVec, pVec) * invDeterminant;
+            if (hitInfo.u < 0 || hitInfo.u > 1) return false;
+            Vector3 qVec = Vector3.Cross(tVec, edge1);
+            hitInfo.v = Vector3.Dot(ray.Direction, qVec) * invDeterminant;
+            if (hitInfo.v < 0 || hitInfo.v + hitInfo.u > 1) return false;
+            hitInfo.t = Vector3.Dot(edge2, qVec) * invDeterminant;
+            return hitInfo.t > 0;
         }
     }
 }
--- Raytracer/Renderer.cs
+++ Raytracer/Renderer.cs
@@ -28,52 +28,9 @@ namespace Raytracer
             // Screen Y is pointed down
             float viewportY = 1 - (screenY + 0.5f) / _pictureHeight * 2;
             var ray = _scene.Camera.ViewportPointToRay(viewportX, viewportY);
-            RaycastHit hitInfo;
-            bool hit = Raycast(_scene.Meshes[0], ray, out hitInfo);
-            if (hit)
-            {
-                return Color.FromArgb((int)(0xFF * (hitInfo.t / 7)), 0, 0);
-            }
-            return Color.White;
-        }
-
-        private bool Raycast(Mesh mesh, Ray ray, out RaycastHit closestHit)
-        {
-            closestHit = new RaycastHit();
-            closestHit.t = float.MaxValue;
-            for (int i = 0; i < mesh.TriangleCount; i++)
-            {
-                RaycastHit hitInfo;
-                if (!RaycastTriangle(mesh, i, ray, out hitInfo) ||
-                    (closestHit.t < hitInfo.t)) continue;
-                closestHit = hitInfo;
-            }
-            return closestHit.t != float.MaxValue;
-        }
-
-        private bool RaycastTriangle(Mesh mesh, int triangleId, Ray ray, out RaycastHit hitInfo)
-        {
-            Vector3[] vertices = mesh.Vertices;
-            int[] triangles = mesh.Triangles;
-            hitInfo = new RaycastHit();
-            int triangleOffset = triangleId * 3;
-            Vector3 vert0 = vertices[triangles[triangleOffset]];
-            Vector3 vert1 = vertices[triangles[triangleOffset + 1]];
-            Vector3 vert2 = vertices[triangles[triangleOffset + 2]];
-            Vector3 edge1 = vert1 - vert0;
-            Vector3 edge2 = vert2 - vert0;
-            Vector3 pVec = Vector3.Cross(ray.Direction, edge2);
-            float determinant = Vector3.Dot(edge1, pVec);
-            if (determinant == 0) return false;
-            float invDeterminant = 1 / determinant;
-            Vector3 tVec = ray.Origin - vert0;
-            hitInfo.u = Vector3.Dot(tVec, pVec) * invDeterminant;
-            if (hitInfo.u < 0 || hitInfo.u > 1) return false;
-            Vector3 qVec = Vector3.Cross(tVec, edge1);
-            hitInfo.v = Vector3.Dot(ray.Direction, qVec) * invDeterminant;
-            if (hitInfo.v < 0 || hitInfo.v + hitInfo.u > 1) return false;
-            hitInfo.t = Vector3.Dot(edge2, qVec) * invDeterminant;
-            return hitInfo.t > 0;
+            Color pixel = Color.White;
+            float distance = _scene.Meshes[0].Raycast(ray, ref pixel, float.MaxValue);
+            return pixel;
         }
     }
 }
