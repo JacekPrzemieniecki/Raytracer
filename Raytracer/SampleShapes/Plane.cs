392ee307f41008784ef494255d1cb7f12ffeaad5
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-27 00:00:32 +0200

    Glossy shader, Plane primitive

---
+++ Raytracer/SampleShapes/Plane.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using Raytracer.Shaders;
+
+namespace Raytracer.SampleShapes
+{
+    class Plane : Mesh
+    {
+        private Color _color = Color.Gray;
+        public Plane(Vector3 position, Vector3 left, Vector3 forward, float edge, Shader shader)
+        {
+            Position = position;
+            Shader = shader;
+            float halfEdge = edge / 2;
+            Vertices = new[]
+            {
+                (forward - left) * halfEdge,
+                (forward + left) * halfEdge,
+                (-1 * forward - left) * halfEdge,
+                (left - forward) * halfEdge
+            };
+            Triangles = new[]
+            {
+                new Triangle(this, 0, 1, 2, _color),
+                new Triangle(this, 1, 3, 2, _color)
+            };
+            Init();
+        }
+    }
+}
---
+++ Raytracer/Shaders/GlossyShader.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace Raytracer.Shaders
+{
+    class GlossyShader : Shader
+    {
+        private float _roughness;
+        private float _oneMinRoughness;
+        public GlossyShader(float roughness)
+        {
+            _roughness = roughness;
+            _oneMinRoughness = 1 - roughness;
+        }
+
+        public override Color Shade(Scene scene, RaycastHit hitInfo, int maxRecursiveRaycasts)
+        {
+            if (maxRecursiveRaycasts == 0) return hitInfo.Triangle.Color;
+            Ray ray = ReflectedRay(hitInfo);
+            Color reflectedColor = scene.SampleColor(ray, maxRecursiveRaycasts - 1);
+            Color triangleColor = hitInfo.Triangle.Color;
+            Color returnColor = Color.FromArgb((int) (triangleColor.R * _roughness + reflectedColor.R * _oneMinRoughness),
+                (int) (triangleColor.G * _roughness + reflectedColor.G * _oneMinRoughness),
+                (int) (triangleColor.B * _roughness + reflectedColor.B * _oneMinRoughness));
+            return returnColor;
+        }
+    }
+}
