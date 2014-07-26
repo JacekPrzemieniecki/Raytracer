10c77b3deb13e44c83e155ead18c7a693c44dd3f
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-07-26 18:42:08 +0200

    Add normal-based shader

---
+++ Raytracer/Shaders/NormalShader.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Raytracer.Shaders
+{
+    class NormalShader : Shader
+    {
+        public override Color Shade(Scene scene, Mesh mesh, RaycastHit hitInfo)
+        {
+            Vector3 normal = mesh.SurfaceNormal(hitInfo);
+            return Color.FromArgb(0x7F + (int) (normal.x * 0x7F), 0x7F + (int) (normal.y * 0x7F), 0x7F + (int) (normal.z * 0x7F));
+        }
+    }
+}
