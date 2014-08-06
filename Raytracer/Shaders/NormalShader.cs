f82b2fc80e9e3269afe4463d4ab0a561a2fea4ff
Jacek Przemieniecki <jacek@przemieniecki.net>
2014-08-06 15:41:04 +0200

    Bugfix for negative vector values

--- Raytracer/Shaders/NormalShader.cs
+++ Raytracer/Shaders/NormalShader.cs
@@ -4,7 +4,7 @@
     {
         public override Vector3 Shade(Scene scene, RaycastHit hitInfo, int maxRecursiveRaycasts)
         {
-            return hitInfo.Triangle.SurfaceNormal(hitInfo.U, hitInfo.V);
+            return hitInfo.Triangle.SurfaceNormal(hitInfo.U, hitInfo.V) * 0.5f + new Vector3(0.5f, 0.5f, 0.5f);
         }
     }
 }
--- Raytracer/Shaders/ReflectedVectorShader.cs
+++ Raytracer/Shaders/ReflectedVectorShader.cs
@@ -5,7 +5,7 @@
         public override Vector3 Shade(Scene scene, RaycastHit hitInfo, int maxRecursiveRaycasts)
         {
             Ray r = ReflectedRay(hitInfo);
-            return r.Direction;
+            return r.Direction * 0.5f + new Vector3(0.5f, 0.5f, 0.5f);
         }
     }
 }
